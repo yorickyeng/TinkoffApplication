24160cd128095b2ce8c97a658decb05b9d0a1710
yorickyeng <sultanovainur05@gmail.com>
2024-10-28 12:41:00 +0300

    added zoo tinkoff

--- zoo tinkoff/src/Main.kt
+++ zoo tinkoff/src/Main.kt
@@ -7,52 +7,63 @@ interface Dog : Animal {
     val biteType: BiteType
 }
 
-interface Cat : Animal {
-    val behaviorType: BehaviorType
-}
-
 enum class BiteType {
     STRAIGHT, OVERBITE, UNDERBITE
+}
+
+interface Cat : Animal {
+    val behaviorType: BehaviorType
 }
 
 enum class BehaviorType {
     ACTIVE, PASSIVE
 }
 
-class Husky(override val weight: Double, override val age: Int) : Dog {
-    override val biteType = BiteType.STRAIGHT
-}
+class Husky(
+    override val weight: Double,
+    override val age: Int,
+    override val biteType: BiteType
+) : Dog
 
-class Corgi(override val weight: Double, override val age: Int) : Dog {
-    override val biteType = BiteType.UNDERBITE
-}
+class Corgi(
+    override val weight: Double,
+    override val age: Int,
+    override val biteType: BiteType
+) : Dog
 
-class ScottishCat(override val weight: Double, override val age: Int) : Cat {
-    override val behaviorType = BehaviorType.PASSIVE
-}
+class ScottishFold(
+    override val weight: Double,
+    override val age: Int,
+    override val behaviorType: BehaviorType
+) : Cat
 
-class SiameseCat(override val weight: Double, override val age: Int) : Cat {
-    override val behaviorType = BehaviorType.ACTIVE
-}
+class Siamese(
+    override val weight: Double,
+    override val age: Int,
+    override val behaviorType: BehaviorType
+) : Cat
 
 class ZooStore {
-    fun identifyAnimal(breed: String, weight: Double, age: Int): Animal? {
-        return when (breed.lowercase()) {
-            "husky" -> Husky(weight, age)
-            "corgi" -> Corgi(weight, age)
-            "scottish cat" -> ScottishCat(weight, age)
-            "siamese cat" -> SiameseCat(weight, age)
-            else -> null
+    fun identifyAnimal(animal: Animal): String {
+        if (animal is Husky || animal is Corgi) {
+            println("Dog")
+        } else if (animal is Siamese || animal is ScottishFold) {
+            println("Cat")
+        }
+
+        return when (animal) {
+            is Husky -> "Husky with weight ${animal.weight} and age ${animal.age}"
+            is Corgi -> "Corgi with weight ${animal.weight} and age ${animal.age}"
+            is ScottishFold -> "Scottish Fold with weight ${animal.weight} and age ${animal.age}"
+            is Siamese -> "Siamese with weight ${animal.weight} and age ${animal.age}"
+            else -> throw IllegalArgumentException("Unknown animal type")
         }
     }
 }
 
 fun main() {
     val zooStore = ZooStore()
-
-    val dog = zooStore.identifyAnimal("Husky", 25.0, 3)
-    println("A dog: ${dog?.javaClass?.simpleName}, Weight: ${dog?.weight}, Age: ${dog?.age}, Bite Type: ${(dog as Dog).biteType}")
-
-    val cat = zooStore.identifyAnimal("Siamese Cat", 4.5, 2)
-    println("A cat: ${cat?.javaClass?.simpleName}, Weight: ${cat?.weight}, Age: ${cat?.age}, Behavior Type: ${(cat as Cat).behaviorType}")
+    val husky = Husky (25.0, 3, BiteType.UNDERBITE)
+    val type = zooStore.identifyAnimal(husky)
+    println(type)
 }
